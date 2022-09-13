81f7260d0daac43a814a9a1711c79ef4bbc83f77
Mutiu Abdulrasaq <83947644+AbdulrasaqM@users.noreply.github.com>
2022-09-13 16:06:43 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
-/**
- * Main - Entry point
- * print a string
- *
- * Return: 0 on success
- */
- 
-int main (void)
-{
-	char *a;
-	int i;
 
-	a = '_putchar';
-	for (i =0; i<=8; i++)
-	{
-		_putchar (a[i]);
-	}
-	_putchar ('\n');
-	return 0;
+/**
+ * main - Entry point
+ *
+ * Return: nothing, return void
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
 }
