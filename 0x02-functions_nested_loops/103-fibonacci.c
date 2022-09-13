08a25f2b96c80c6771c59567163965f28725f850
Mutiu Abdulrasaq <83947644+AbdulrasaqM@users.noreply.github.com>
2022-09-13 20:28:52 +0100

    Create 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - sum even fibonacci numbers under 4 million.
+ * Return: void
+ */
+
+int main(void)
+{
+	unsigned long n, i, j, fibon, sums;
+
+	i = sums = 0;
+	j = 1;
+	for (n = 0; n < 50; n++)
+	{
+		fibo = i + j;
+		i = j;
+		j = fibon;
+		if (fibon % 2 == 0 && fibon < 4000000)
+		{
+			sums += fibon;
+		}
+	}
+	printf("%lu\n", sums);
+	return (0);
+}
