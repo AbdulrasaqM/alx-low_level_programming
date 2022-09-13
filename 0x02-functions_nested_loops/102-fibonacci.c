0a925393ba86c192f078c8659435f25e5b19537e
Mutiu Abdulrasaq <83947644+AbdulrasaqM@users.noreply.github.com>
2022-09-13 20:26:02 +0100

    Create 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - print the first 50 fibonacci numbers.
+ * Return: void.
+ */
+int main(void)
+{
+	unsigned long n, i, j, fibon;
+
+	i = 0;
+	j = 1;
+	for (n = 0; n < 50; n++)
+	{
+		fibon = i + j;
+		i = j;
+		j = fibo;
+		printf("%lu", fibon);
+		if (n == 49)
+			putchar('\n');
+		else
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
