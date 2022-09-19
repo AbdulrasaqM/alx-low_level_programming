7f5c085b98d75cc17cc7c98a3bd3ff87a35ea385
Mutiu Abdulrasaq <83947644+AbdulrasaqM@users.noreply.github.com>
2022-09-19 18:59:47 +0100

    betty doc update 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,14 +2,15 @@
 
 /**
  *swap_int - this swpas two integers
- *@a - A pointer will be updated with the B value
- *@b - B Pointer will be updated with the A value
+ *@a: - A pointer will be updated with the B value
+ *@b: - B Pointer will be updated with the A value
  *Return void
  */
 
 void swap_int(int *a, int *b)
 {
 int swap;
+  
 swap = *a;
 *a = *b;
 *b = swap;
