d2eb761403ce2e2de07f490e5115d710966a531b
Mutiu Abdulrasaq <83947644+AbdulrasaqM@users.noreply.github.com>
2022-10-03 18:37:46 +0100

    Update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,6 +5,11 @@
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char **strtow(char *str);
+int _putchar(char c);
+int string_len(char *str);
+char *truncate_extra_spaces(char *str1, char *str);
+char **mem_alloc(char *str1);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
